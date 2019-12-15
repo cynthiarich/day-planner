3bbf563767ad48585835b745861f33b1b96f9fbe
Cynthia Rich <drichsgirl@gmail.com>
2019-12-15 15:54:58 -0500

    handle past present future formatting

--- assets/script.js
+++ assets/script.js
@@ -14,17 +14,18 @@ function initializeDay (day) {
       calendar = JSON.parse(localStorage.getItem(day));
       console.log("initialize day calendar: " + calendar)
     }
+    //get current time
 
     //create time blocks
     for (var i = 0; i < lengthofday; i++){
       dispTime = startTime + i;
       
-      var group = $("<div>").attr("class", "input-group input-group-lg mb-2");
+      var group = $("<div>").attr("class", "input-group input-group-lg mb-2"); ;
       $("#timeblocks").append(group);
 
       var showTime = $("<div>").attr("class", "input-group-prepend");
       group.append(showTime);
-      var thisTime = $("<span>").attr("class", "input-group-text");
+      var thisTime = $("<span>");
       thisTime.attr("id", "inputGroup-sizing-lg");
       thisTime.text(dispTime + ":00");
       showTime.append(thisTime);
@@ -41,12 +42,35 @@ function initializeDay (day) {
 
       var btnDiv = $("<div>").attr("class", "input-group-append");
       group.append(btnDiv);
-      var sBtn = $("<button>").attr("class", "btn btn-outline-secondary");
+      var sBtn = $("<button>");
       sBtn.attr("type", "button");
       sBtn.attr("id", ("sBtn" + dispTime));
       sBtn.attr("value", dispTime);
       sBtn.text("Save");
       btnDiv.append(sBtn);
+
+      //format based on past, present or future
+      if (dispTime <= 9){
+        dispTime = "0" + dispTime;
+      }
+      var formDay = day + " " + dispTime;
+
+      if (moment(formDay, "MMM Do YYYY HH").isBefore(moment(), 'hour')){
+        thisTime.attr("class", "input-group-text");
+        eventInput.attr("class", "form-control past");
+        sBtn.attr("class", "btn btn-outline-secondary");
+      }
+      else if (moment(formDay, "MMM Do YYYY HH").isSame(moment(), 'hour')){
+        thisTime.attr("class", "input-group-text present");
+        eventInput.attr("class", "form-control present");
+        sBtn.attr("class", "btn btn-outline-primary");
+      }
+      else {
+        thisTime.attr("class", "input-group-text future");
+        eventInput.attr("class", "form-control");
+        sBtn.attr("class", "btn btn-outline-primary");
+      }
+
 
       $("#sBtn" + dispTime).on("click", function () {
         updateLocalStorage(day, $(this).attr("value"), $(this).parent().parent().find("input").val())
